ea81e0d91659d4a694686630f98728614c36b72b
Cédric Bosdonnat <cbosdonnat@suse.com>
2024-05-15 15:05:39 +0200

    Fix status column to display gitsigns again

--- nvim/lua/plugins/colorscheme.lua
+++ nvim/lua/plugins/colorscheme.lua
@@ -82,9 +82,12 @@ return {
             local builtin = require("statuscol.builtin")
             require("statuscol").setup({
                 segments = {
+                    { text = { builtin.foldfunc }, click = "v:lua.ScFa" },
+                    { text = { "%s" }, click = "v:lua.ScSa" },
                     {
-                        text = { builtin.foldfunc },
-                        click = "v:lua.ScFa",
+                      text = { builtin.lnumfunc, " " },
+                      condition = { true, builtin.not_empty },
+                      click = "v:lua.ScLa",
                     },
                 },
             })
