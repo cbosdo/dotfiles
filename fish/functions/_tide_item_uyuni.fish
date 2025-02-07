2e93735eee5bf32f096067d8721ee8d2130e7e05
Cédric Bosdonnat <cbosdonnat@suse.com>
2025-02-07 17:51:38 +0100

    Shorten uyuni paths in fish tide item

--- fish/functions/_tide_item_uyuni.fish
+++ fish/functions/_tide_item_uyuni.fish
@@ -1,6 +1,9 @@
 function _tide_item_uyuni
     if set -q OBS_PROJ
-        _tide_print_item uyuni $tide_uyuni_icon' ' $OBS_PROJ
+        set proj (string replace -r 'home:([^:]+):branches' '$1' $OBS_PROJ)
+        set proj (string replace 'Devel:Galaxy:Manager' Manager $proj)
+        set proj (string replace 'systemsmanagement:Uyuni' Uyuni $proj)
+        _tide_print_item uyuni $tide_uyuni_icon' ' $proj
     end
 end
 set -U tide_uyuni_icon ''
